a2805f6cfd03f33c3ab604e3b5370855c8191f23
Mohamed-Moussad <eng.mos3d1@gmail.com>
2024-09-18 21:30:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import pandas as pd
 import plotly.express as px
 st.header('file upload app 2')
@@ -9,7 +8,6 @@ if file is not None:
 
 st.write(df)
 
-
 num_row=st.slider('choose num rows', min_value=1,max_value=len(df),step=1)
 names_column=st.multiselect('Choose names of columns',df.columns.to_list())
 st.write(df[:num_row][names_column])
@@ -18,10 +16,18 @@ if names_column:
   st.write(df[:num_row][names_column])
 else:
   st.write(df[:num_row])
-
 num_col = df.select_dtypes(include='number').columns.to_list()
-x_col=st.selectbox('choose x column',num_col)
-y_col=st.selectbox('choose y column',num_col)
-Color =st.selectbox('choose color',df.columns.to_list())
+col1,col2,col3 =st.columns(3)
+with col1:
+  x_col=st.selectbox('choose x column',num_col)
+with col2:
+  y_col=st.selectbox('choose y column',num_col)
+with col3:
+  Color =st.selectbox('choose color',df.columns.to_list())
+
+
+
+
+
 fig = px.scatter(df,x=x_col,y=y_col,color = Color)
 st.plotly_chart(fig)
